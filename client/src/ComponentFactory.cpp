7a9069edd2171699544c36a12de6eaa95d6fbee7
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-11-06 15:46:04 -0500

    Update ComponentFactory.cpp

--- client/src/ComponentFactory.cpp
+++ client/src/ComponentFactory.cpp
@@ -1,19 +1,70 @@
 #include "ComponentFactory.h"
 
+SerializerUniquePtr ComponentFactory::create(SerializerType type)
+{
+	switch (type)
+	{
+	case SerializerType::BINARY:
+		return std::make_unique<BinarySerializer>();
+	case SerializerType::JSON:
+		// return std::make_unique<JSON_TODO>();
+	default:
+		return nullptr;
+	}
+}
 
+EncoderUniquePtr ComponentFactory::create(EncoderType type)
+{
+	switch (type)
+	{
+	case EncoderType::BASE64:
+		return std::make_unique<Base64>();
+	case EncoderType::HEX:
+		// return std::make_unique<HEX_TODO>();
+	default:
+		return nullptr;
+	}
+}
+
+EncryptorUniquePtr ComponentFactory::create(EncryptorType type)
+{
+	switch (type)
+	{
+	case EncryptorType::XOR:
+		return std::make_unique<XOR>();
+	case EncryptorType::AES:
+		// return std::make_unique<AES_TODO>();
+	default:
+		return nullptr;
+	}
+}
 
 TransportLayerUniquePtr TransportLayerFactory::create(
 		MessageHandler& messageHandler, 
 		const std::string& host, 
 		const std::string& port, 
-		TransportLayerType type)
+		TransportLayerType type,
+		SerializerType serializerType,
+		EncoderType encoderType,
+		EncryptorType encryptorType)
 {
+
+	auto serializer = ComponentFactory::create(serializerType);
+	auto encoder = ComponentFactory::create(encoderType);
+	auto encryptor = ComponentFactory::create(encryptorType);
+	
 	switch (type)
 	{
 		//case NONE:
 		//break;
 		case TransportLayerType::TCP:
-			return std::make_unique<TCPTransportLayer>(messageHandler, host, port);
+			return std::make_unique<TCPTransportLayer>(
+					messageHandler, 
+					host, 
+					port,
+					std::move(serializer),
+					std::move(encoder),
+					std::move(encryptor));
 			/*
 			   case UDP:
 			   return std::make_unique<UDPTransportLayer>(messageHandler, host, port);
@@ -26,7 +77,9 @@ TransportLayerUniquePtr TransportLayerFactory::create(
 			   */
 		default:
 			// throw error
-			break;
+			//break;
+			std::cout << "throw error" << std::endl;
+			return nullptr;
 
 	}
 }
