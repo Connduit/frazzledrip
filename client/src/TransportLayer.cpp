0d2fe4d1e332ec5e6e18791937b6c12ef9d26ce3
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-11-06 16:02:03 -0500

    Update TransportLayer.cpp

--- client/src/TransportLayer.cpp
+++ client/src/TransportLayer.cpp
@@ -75,8 +75,27 @@ uint32_t TransportLayer::generateId()
 
 
 //TCPTransportLayer::TCPTransportLayer(MessageHandler* hdlr, const std::string& server, uint16_t port)
-TCPTransportLayer::TCPTransportLayer(MessageHandler& messageHandler, const std::string& server, std::string port)
-    : TransportLayer(messageHandler), server_(server), port_(port)
+TCPTransportLayer::TCPTransportLayer(
+		MessageHandler& messageHandler, 
+		const std::string& server, 
+		const std::string& port,
+		SerializerUniquePtr serializer,
+		EncoderUniquePtr encoder,
+		EncryptorUniquePtr encryptor,
+		SerializerType serializerType,
+		EncoderType encoderType,
+		EncryptorType encryptorType)
+    	: 
+		TransportLayer(
+			messageHandler,
+			std::move(serializer),
+			std::move(encoder),
+			std::move(encryptor),
+			serializerType,
+			encoderType,
+			encryptorType),
+		server_(server), 
+		port_(port)
 {
     initializeWinsock();
 }
