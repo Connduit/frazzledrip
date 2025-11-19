2d652e5fad0c14a6bad4dbfbae54816538dc96c0
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-11-19 17:10:04 -0500

    Implement Dispatcher class for message dispatching
    Added Dispatcher class for message handling.

--- client/include/Dispatcher.h
+++ client/include/Dispatcher.h
@@ -1,1 +1,10 @@
 // message dispatcher... where messages should go
+class Dispatcher
+{
+public:
+    using Handler = std::function<void(const InternalMessage&)>;
+    void registerHandler(MessageType type, Handler handler);
+    void dispatch(const InternalMessage& msg);
+private:
+    std::unordered_map<MessageType, Handler> handlers_;
+};
