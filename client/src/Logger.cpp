5eb724066c049460368b0e6f7015d81778b95216
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-11-20 11:02:25 -0500

    Update Logger.cpp

--- client/src/Logger.cpp
+++ client/src/Logger.cpp
@@ -1,21 +1,61 @@
 #include "Logger.h"
-Logger& Logger::Instance() {
+
+#ifdef _DEBUG
+
+Logger::Logger()
+    : output_(&std::cout)   // default output is console
+{
+}
+
+Logger& Logger::instance()
+{
     static Logger instance;
     return instance;
 }
 
-Logger::Logger() : out_(&std::cout) {}
+void Logger::setOutput(std::ostream* out)
+{
+    if (out)
+        output_ = out;
+}
 
-void Logger::SetOutputFile(const std::string& filename) {
-    file_.open(filename);
-    if (file_.is_open()) {
-        out_ = &file_;
+const char* Logger::levelToString(Level level) const
+{
+    switch (level) {
+        case Level::Trace:    return "TRACE";
+        case Level::Debug:    return "DEBUG";
+        case Level::Info:     return "INFO";
+        case Level::Warn:     return "WARN";
+        case Level::Error:    return "ERROR";
+        case Level::Critical: return "CRITICAL";
+        default:              return "UNKNOWN";
     }
 }
 
-void Logger::Log(LogLevel level, const std::string& msg,
-                 const char* file, int line)
+std::string Logger::currentTimestamp() const
 {
-    (*out_) << "[" << file << ":" << line << "] "
-            << msg << std::endl;
+    auto t = std::time(nullptr);
+    std::tm tm{};
+#ifdef _WIN32
+    localtime_s(&tm, &t);
+#else
+    localtime_r(&t, &tm);
+#endif
+
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+    return oss.str();
 }
+
+void Logger::log(Level level,
+                 const std::string& msg,
+                 const char* file,
+                 int line)
+{
+    (*output_) << "[" << currentTimestamp() << "] "
+               << "[" << levelToString(level) << "] "
+               << file << ":" << line << " - "
+               << msg << std::endl;
+}
+
+#endif // _DEBUG
