e1ee736cc6b1ce224265b45d97877cb3c1f6da81
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-11-20 17:47:23 -0500

    Update Config.h

--- client/include/Config.h
+++ client/include/Config.h
@@ -1,1 +1,18 @@
 // config.h 
+
+typedef struct Config
+{
+        Config() :
+transportLayerType_(TransportLayerType::TCP),
+                serializerType_(SerializerType::BINARY),
+                encoderType_(EncoderType::BASE64),
+encryptorType_(EncryptorType::XOR)
+        {}
+
+        TransportLayerType transportLayerType_;
+SerializerType serializerType_;
+EncoderType encoderType_;
+EncryptorType encryptorType_;
+       
+
+} Config;
