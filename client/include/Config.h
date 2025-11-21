f87284e3e63bf21140308b8ee1ccd79a5506fdbc
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-11-21 09:25:34 -0500

    Update Config.h

--- client/include/Config.h
+++ client/include/Config.h
@@ -1,18 +1,25 @@
-// config.h 
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "MessageTypes.h"
 
 typedef struct Config
 {
         Config() :
-transportLayerType_(TransportLayerType::TCP),
+                transportLayerType_(TransportLayerType::TCP),
                 serializerType_(SerializerType::BINARY),
                 encoderType_(EncoderType::BASE64),
-encryptorType_(EncryptorType::XOR)
-        {}
+                encryptorType_(EncryptorType::XOR)
+                {}
 
+        // int server_;
+        // int port_;
         TransportLayerType transportLayerType_;
-SerializerType serializerType_;
-EncoderType encoderType_;
-EncryptorType encryptorType_;
+        SerializerType serializerType_;
+        EncoderType encoderType_;
+        EncryptorType encryptorType_;
        
 
 } Config;
+
+#endif
