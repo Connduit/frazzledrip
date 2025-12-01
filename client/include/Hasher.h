c589f075c334f5ab35f2a52c9e31aeefd979030c
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-12-01 12:35:43 -0500

    Update and rename Hash.h to Hasher.h

--- client/include/Hasher.h
+++ client/include/Hasher.h
@@ -1,4 +1,4 @@
-/* hash.h */
+/* Hasher.h */
 /* hashing is used to store passwords securely and verify messages haven't changed */
 
 // TODO: hashes should be a part of a namespace? maybe api namespace? or "precompiled" namespace
