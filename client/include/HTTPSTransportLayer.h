552a24e68cba2fcd0fbccf6fb35bd02c80eb32f5
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-12-02 12:55:19 -0500

    Update HTTPSTransportLayer.h

--- client/include/HTTPSTransportLayer.h
+++ client/include/HTTPSTransportLayer.h
@@ -5,10 +5,41 @@
 
 class HTTPSTransportLayer : public TransportLayer
 {
-public:
+	HTTPSTransportLayer(ApiManager* apiManager);
+	HTTPSTransportLayer(ApiManager* apiManager, std::string& server, std::string& port);
+	~HTTPSTransportLayer();
+
+	// Attemps to send a std::vector<uint8_t> as a raw buffer to server
+	bool send(const RawByteBuffer& msg);
+
+	// Creates socket and attempts to connect to it
+	// Returns true on success
+	bool connect();
+
+	// Receives raw buffer from server
+	// Returns raw buffer as a std::vector<uint8_t>
+	void receive();
+
+	// Getter to see if we've connected to the server
+	bool isConnected() { return connected_; } // TODO: move to parent class? 
+
+	void run();
+
+	// call this function manually in main loop instead of using threads
+	// void update();
+
 private:
-	// encryptor object for https tls encryption (not for encrypting the InternalMessage)
-	Encryptor* encryptor_;
+	// Initializes WSADATA by being called through the constructor 
+	bool initializeWinsock();
+
+	// TODO: move this to abstract class?
+	std::string server_;
+	std::string port_;
+	SOCKET socket_ = INVALID_SOCKET;
+	bool connected_ = false;
+	//std::atomic<bool> connected_;
+
+	ApiManager* apiManager_;
 };
 
 #endif
