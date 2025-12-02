8c6c3566b24a42936db1154d8813fbaf98654d58
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-12-02 13:28:03 -0500

    Update HTTPSTransportLayer.cpp

--- client/src/HTTPSTransportLayer.cpp
+++ client/src/HTTPSTransportLayer.cpp
@@ -1,5 +1,31 @@
 #include "HTTPSTransportLayer.h"
 
+HTTPSTransportLayer::HTTPSTransportLayer(
+    ApiManager* apiManager)
+    :
+    apiManager_(apiManager),
+    TransportLayer()
+{
+    // TODO: initialize... WinHttpOpen? 
+}
+
+HTTPSTransportLayer::HTTPSTransportLayer(
+    ApiManager* apiManager, 
+    std::string& server, 
+    std::string& port)
+    : 
+    apiManager_(apiManager),
+    server_(server),
+    port_(port)
+{
+    // TODO: initialize... WinHttpOpen?
+}
+
+HTTPSTransportLayer::~HTTPSTransportLayer()
+{
+    // TODO: WinHttpCloseHandle(TODO_MY_HANDLE);
+}
+
 bool HTTPSTransportLayer::connect()
 {
     HINTERNET hConnect = WinHttpConnect(hSession, L"example.com", INTERNET_DEfAULT_HTTPS_PORT, 0);
