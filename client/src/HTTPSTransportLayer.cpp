ffd575d6c6d453c2b913f9989dff4f306c25c3d0
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-12-02 14:21:59 -0500

    Update HTTPSTransportLayer.cpp

--- client/src/HTTPSTransportLayer.cpp
+++ client/src/HTTPSTransportLayer.cpp
@@ -107,6 +107,6 @@ void HTTPSTransportLayer::receive()
 
         buffer.resize(bytesRead);
 
-        receiveCallback_(buffer);
+        receiveCallback_(buffer); // TODO: need a way to handle muliple chunks and putting them back together if the enitre message isn't sent in a single chunk
     }
 }
