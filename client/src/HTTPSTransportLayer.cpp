235452e9957dd87bbdeda75e25398775d421e1fe
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-12-02 13:14:16 -0500

    Update HTTPSTransportLayer.cpp

--- client/src/HTTPSTransportLayer.cpp
+++ client/src/HTTPSTransportLayer.cpp
@@ -1,1 +1,7 @@
 #include "HTTPSTransportLayer.h"
+
+bool HTTPSTransportLayer::connect()
+{
+    HINTERNET hConnect = WinHttpConnect(hSession, L"example.com", INTERNET_DEfAULT_HTTPS_PORT, 0);
+    return true;
+}
