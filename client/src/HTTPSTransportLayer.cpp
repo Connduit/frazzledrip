dbee3ccf49108e7b623a6073d69c8ee8afb4ddeb
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-12-02 13:56:49 -0500

    Update HTTPSTransportLayer.cpp

--- client/src/HTTPSTransportLayer.cpp
+++ client/src/HTTPSTransportLayer.cpp
@@ -28,6 +28,20 @@ HTTPSTransportLayer::~HTTPSTransportLayer()
 
 bool HTTPSTransportLayer::connect()
 {
-    HINTERNET hConnect = WinHttpConnect(hSession, L"example.com", INTERNET_DEfAULT_HTTPS_PORT, 0);
+    hSession_ = WinHttpOpen(L"AgentName/1.0", WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0); // use WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY on win8.1 and newer; otherwise, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY
+    if (!hSession_)
+    {
+        return false;
+    }
+    
+    //HINTERNET hConnect = WinHttpConnect(hSession, L"example.com", INTERNET_DEfAULT_HTTPS_PORT, 0);
+    hConnect_ = WinHttpConnect(hSession_, L"example.com", INTERNET_DEFAULT_PORT, 0); // INTERNET_DEFAULT_PORT is 80 for http and 443 for https
+
+    if (!hConnect_)
+    {
+        return false;
+    }
+
+    connected_ = true;
     return true;
 }
