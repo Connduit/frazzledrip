fdae9185d57a1d5c53df28a424ddd7f6abc9a324
Connduit <fuxkvalve@gmail.com>
2025-12-03 18:30:24 -0500

    header files

--- client/include/Hasher.h
+++ client/include/Hasher.h
@@ -4,6 +4,8 @@
 /* hashing is used to store passwords securely and verify messages haven't changed */
 
 // TODO: hashes should be a part of a namespace? maybe api namespace? or "precompiled" namespace
+#include <cstdint>
+#include <string_view>
 
 struct FNV1a32Tag {};
 template<typename Tag>
